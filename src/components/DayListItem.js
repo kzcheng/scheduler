518cf24f028d99e5e2fd61ba8d2a2c4e9ca239d4
kzcheng <kevinzifancheng@gmail.com>
2020-04-13 16:57:35 -0700

    Completed DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else {
+      return spots === 1
+        ? `${spots} spot remaining`
+        : `${spots} spots remaining`;
+    }
+  };
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props && props.selected,
+    "day-list__item--full": props && props.spots === 0
+  });
+
+  return (
+    <li
+      className={dayClass}
+      onClick={() => props && props.setDay && props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{props && props.name}</h2>
+      <h3 className="text--light">{props && formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
