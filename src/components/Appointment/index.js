02524a6d81f986455c7b21a6dcc7ecfa17f41814
kzcheng <kevinzifancheng@gmail.com>
2020-04-14 16:38:59 -0700

    Created index of Appointments

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classnames from "classnames";
+
+import "components/Appointment/styles.scss";
+
+export default function Appointment(props) {
+
+
+  return (
+    <article className="appointment">
+      
+    </article>
+  );
+}
