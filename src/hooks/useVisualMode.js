327bb73c69ba75d0eb1420cc8909e6cfe0719548
kzcheng <kevinzifancheng@gmail.com>
2020-04-16 19:30:43 -0700

    Removed useless test code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,61 +6,49 @@ export default function useVisualMode(initial) {
   // A No, history must be a state to be remembered.
   //   Everything you see in this function only runs once when initializing an instance of a hook.
 
-  //TODO Consider renaming this hook to something like useAppointmentMode to make the definition easier to understand
-
-  //TODO Also, I may need to rewrite this function a bit on how it works.
-  //     I remember there were some bugs for the history system.
-  //     A rewrite might be required.
-
   // The states can be considered as the "main public variables" of this class.
   // As in, they need to be accessed from outside this class thing.
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const logHistory = function() {
-    let output = "";
-    for (const entry of history) {
-      output += entry + ", ";
-    }
-    // output = output.substring(0, -2);
-    console.log("History is " + output);
-  };
-
-  // const transition = function(newMode, shouldReplace = false) {
-  //   if (shouldReplace) {
-  //     history.pop();
+  // Useless debug function
+  // const logHistory = function() {
+  //   let output = "";
+  //   for (const entry of history) {
+  //     output += entry + ", ";
   //   }
-
-  //   setMode(newMode);
-  //   setHistory(prev => [...history, newMode]);
+  //   // output = output.substring(0, -2);
+  //   console.log("History is " + output);
   // };
 
+  /**
+   * Transition into another mode.
+   *
+   * @param {*} newMode
+   * @param {*} shouldReplace - Set true when the transition should replace the last mode in history
+   */
   const transition = function(newMode, shouldReplace = false) {
-    // console.log(`newMode is ` + newMode);
-    
     setMode(newMode);
-    console.log("Current Mode:", mode);
-    console.log("New Mode:", newMode);
 
     if (shouldReplace) {
       setHistory(prev => [...prev.slice(0, -1), newMode]);
     } else {
       setHistory(prev => [...prev, newMode]);
     }
-
-    console.log("History:", history);
-    
-    
-    // setTimeout(() => {
-    // }, 1000);
   };
 
+  /**
+   * Go back to the previous mode.
+   */
   const back = function() {
-    if (history.length <= 1) {
+    // Can't go back if there isn't any history.
+    if (h.length <= 1) {
       return;
     }
 
+    // Go back to the second last mode of the history
     setMode(history[history.length - 2]);
+    // Remove the last mode of history
     setHistory(history.slice(0, -1));
   };
 
