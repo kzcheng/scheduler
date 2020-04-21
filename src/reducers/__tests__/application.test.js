8b36c90e72b8d63c3780b3b30c5302da888a5158
kzcheng <kevinzifancheng@gmail.com>
2020-04-20 20:38:09 -0700

    Deleted all __tests__

--- src/reducers/__tests__/application.test.js
+++
@@ -1,9 +0,0 @@
-import reducer from "reducers/application";
-
-describe("Application Reducer", () => {
-  it("throws an error with an unsupported type", () => {
-    expect(() => reducer({}, { type: null })).toThrowError(
-      /tried to reduce with unsupported action type/i
-    );
-  });
-});
